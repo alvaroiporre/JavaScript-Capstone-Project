2a3ba32c719323e0d7a9b158e31f766f16146b2e
alvaroiporre <alvaroiporremartinez@gmail.com>
2023-05-22 21:57:25 -0400

    Add call to API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './index.css';
+
+const title = window.document.getElementById('tv-show-title');
+
+let data = [];
+
+await fetch('https://api.tvmaze.com/shows')
+  .then((response) => response.json())
+  .then((json) => {
+    data = json;
+  });
+
+title.innerText = data[0].name;
